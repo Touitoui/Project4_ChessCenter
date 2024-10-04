3ffe3c2969a043646572b39a22d1e2dafe8e911f
Mickael <mickael.gresse@gmail.com>
2024-10-04 09:49:14 +0200

    added main to start script

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+import json
+
+from app.controller.tournament_controller import TournamentController
+
+# TODO: add tools, check if required folders exists
+controller = TournamentController()
+controller.main()
