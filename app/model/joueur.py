87b043c6073aaefab6eeff97815a07b392583b5d
Mickael <mickael.gresse@gmail.com>
2024-10-04 09:53:14 +0200

    init now create an empty player, new player adapted to take only one dict

--- app/model/joueur.py
+++ app/model/joueur.py
@@ -5,38 +5,42 @@ from os.path import isfile, join
 
 player_folder = 'data/players/'
 
+# from datetime import date
+#
+#
+# def calculate_age(born):
+#     today = date.today()
+#     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
 
 class Player:
     players = {}
     next_id = 0
 
-    def __init__(self, file_name):  # last_name, first_name, birthdate, club_id=0):
+    def __init__(self):  # , file_name):  # last_name, first_name, birthdate, club_id=0):
         self.id = 0
         self.last_name = None
         self.first_name = None
         self.birthdate = None
         self.club_id = None
         self.score = 0
-        self._load_player(file_name)
+        # self._load_player(file_name)
 
-    def new_player(self, last_name, first_name, birthdate, club_id):
+    def new_player(self, data):
         self.id = self.next_id
         self.next_id += 1
-        self.last_name = last_name
-        self.first_name = first_name
-        self.birthdate = birthdate
-        self.club_id = club_id
+        self.last_name = data["last_name"]
+        self.first_name = data["first_name"]
+        self.birthdate = data["birthdate"]
+        self.club_id = data["club_id"]
 
-    # def victory(self):
-    #     self.score += 1
-    #
-    # def draw(self):
-    #     self.score += 0.5
+        self.players[self.id] = self
+        self.save_player()
 
     def add_score(self, score):
         self.score += score
 
-    def _load_player(self, path):
+    def load_player(self, path):
         with open(path, 'r', encoding='utf-8') as f:
             data = json.load(f)
             self.id = data["id"]
@@ -46,15 +50,30 @@ class Player:
             self.club_id = data["club_id"]
 
     def save_player(self):
+        data = {"id": self.id,
+                "last_name": self.last_name,
+                "first_name": self.first_name,
+                "birthdate": self.birthdate,
+                "club_id": self.club_id}
         path = os.path.join(player_folder, str(self.id) + ".json")
         with open(path, 'w', encoding='utf-8') as f:
-            json.dump(vars(self), f, ensure_ascii=False, indent=4)
+            json.dump(data, f, ensure_ascii=False, indent=4)
 
     def get_details(self):
         return [value for attr, value in vars(self).items() if not callable(value) and not attr.startswith("__")]
 
     def get_full_name(self):
         return self.last_name + " " + self.first_name
+
+    @classmethod
+    def describe(cls, id_):
+        player = cls.players[id_]
+        text = "ID: " + str(id_) + '\n'
+        text += "Nom: " + player.last_name + '\n'
+        text += "Prénom: " + player.first_name + '\n'
+        text += "Age: " + "" + " (" + player.birthdate + ")\n"
+        text += "ID Club: " + player.club_id + '\n'
+        return text
 
     @classmethod
     def get_loaded_players(cls):
@@ -66,7 +85,7 @@ class Player:
         player_id = list(cls.players.keys())
         player_text = []
         for id_ in player_id:
-            player_text.append(str(id_) + "\t" + cls.players[id_].last_name + " " + cls.players[id_].first_name)
+            player_text.append(cls.players[id_].last_name + " " + cls.players[id_].first_name)
         return player_id, player_text
 
     @classmethod
@@ -78,7 +97,14 @@ class Player:
             if player_id >= cls.next_id:
                 cls.next_id = player_id + 1
             file_path = os.path.join(player_folder, player)
-            cls.players[player_id] = Player(file_path)
+            cls.players[player_id] = Player()
+            cls.players[player_id].load_player(file_path)
+
+    @classmethod
+    def use_next_id(cls):
+        id_ = cls.next_id
+        cls.next_id += 1
+        return id_
 
 
 Player.load_all_players()
