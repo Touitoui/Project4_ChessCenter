8335880c9dcee5a6069d460fed72d45ff33f4236
Mickael <mickael.gresse@gmail.com>
2024-10-08 11:58:25 +0200

    main now handle data folder creation

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import random
 import json
-
 from app.controller.tournament_controller import TournamentController
+from app.tools.utils import create_folder
 
-# TODO: add tools, check if required folders exists
-controller = TournamentController()
-controller.main()
+
+folders = {
+    "data": "data",
+    "players": "data/players/",
+    "tournaments": "data/tournaments/",
+}
+
+
+def main():
+    for folder in folders:
+        create_folder(folder)
+    # TODO: cleanup tool, reports
+    # TODO: Support de presentation
+    controller = TournamentController()
+    controller.main()
+
+
+if __name__ == '__main__':
+    main()
